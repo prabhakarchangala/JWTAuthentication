64746cf6e80035d8aacc8b353b85a15ce8a1bb65
Prabhakar Changala <prabha.uu3@gmail.com>
2023-08-25 21:50:41 +0530

    other changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { JwtModule } from "@auth0/angular-jwt";
+import { AuthGuard } from './guards/auth-guard.service';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { ToastrModule } from 'ngx-toastr';
+import { ProductComponent } from './product/product.component';
+
+//all components routes
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'product', component: ProductsComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+];
+
+//function is use to get jwt token from local storage
+export function tokenGetter() {
+  return localStorage.getItem("jwt");
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HomepageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["localhost:7299"],
+        disallowedRoutes: []
+      }
+  }),
+  ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
